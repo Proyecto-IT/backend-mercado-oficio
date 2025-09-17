f6649baee14371aaf1bac3b5a9e20760fe8cf9e9
ElZorro <thiago2007crackz@gmail.com>
2025-09-17 14:12:01 -0300

    Creacion de proyecto

---
+++ mercado-oficio/src/main/java/com/proyecto_it/mercado_oficio/MercadoOficioApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto_it.mercado_oficio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoOficioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoOficioApplication.class, args);
+	}
+
+}
---
+++ mercado-oficio/src/test/java/com/proyecto_it/mercado_oficio/MercadoOficioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_it.mercado_oficio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoOficioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
